3e93968506e5881eefd8cd3ab92d381b2412368e
Titus Herdiawan <76503729+titusherd@users.noreply.github.com>
2024-04-15 22:58:26 +0700

    fix: image size

--- javascript/e-commerce/main.js
+++ javascript/e-commerce/main.js
@@ -12,22 +12,29 @@ async function buildProducts() {
     console.log(productContainer);
     products.forEach(product => {
         const productCard = document.createElement('div')
-        productCard.classList.add('border-2', 'flex', 'flex-col', 'gap-2', 'w-56', 'justify-center', 'p-2')
+        productCard.classList.add('border-2', 'flex', 'flex-col', 'gap-2', 'w-56', 'justify-center', 'p-2', 'border-gray-200', 'rounded-lg')
 
         const productImageDiv = document.createElement('div')
         productImageDiv.classList.add('flex', 'justify-center', 'p-4', 'border-2', 'rounded-lg')
         const productImage = document.createElement('img')
         productImage.src = product.image
-        productImage.classList.add('w-56', 'h-56')
+        productImage.classList.add('w-full', 'h-40', 'object-cover')
         productImageDiv.append(productImage)
 
+        const productDetails = document.createElement('div')
+        productDetails.classList.add('flex', 'flex-col', 'gap-2', 'p-2')
+
         const productName = document.createElement('h3')
-        productName.textContent = product.title
+        productName.textContent = product.title.slice(0, 16)
+        productName.classList.add('text-lg', 'font-semibold')
 
         const productPrice = document.createElement('p')
-        productPrice.textContent = product.price
+        productPrice.textContent = `$ ${product.price}`
+        productPrice.classList.add('text-red-500', 'font-bold')
 
-        productCard.append(productImageDiv, productName, productPrice)
+        productDetails.append(productName, productPrice)
+
+        productCard.append(productImageDiv, productDetails)
         productContainer.append(productCard)
 
 
