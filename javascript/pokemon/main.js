c9e65b773eb554aef4f2aaa328ff684e725ee765
Titus Herdiawan <76503729+titusherd@users.noreply.github.com>
2024-04-16 19:04:29 +0700

    feat: buildApp

--- javascript/pokemon/main.js
+++ javascript/pokemon/main.js
@@ -1,7 +1,22 @@
 async function getData() {
     const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10');
     const data = await res.json();
-    console.log(data);
+    // console.log(data);
+    return data;
 }
 
-getData();
+// getData();
+
+async function buildApp() {
+    const data = await getData();
+    // console.log(pokemons);
+    const pokemons = data.results;
+
+    pokemons.forEach((pokemon, index) => {
+        console.log(pokemon.name);
+        console.log(`https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${index + 1}.svg`);
+
+    });
+}
+
+buildApp();
