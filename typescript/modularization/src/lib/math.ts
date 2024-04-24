bcd1036d7dbf5dd2345357659052510f053ff92c
Titus Herdiawan <76503729+titusherd@users.noreply.github.com>
2024-04-24 11:05:19 +0700

    learn using module

--- typescript/modularization/src/lib/math.ts
+++
@@ -1,9 +0,0 @@
-function sum(a: number, b: number): number {
-  return a + b;
-}
-
-function sub(a: number, b: number): number {
-  return a - b;
-}
-
-module.exports = { sum, sub };
---
+++ typescript/modularization/src/main.js
@@ -0,0 +1,7 @@
+// const math = require("./lib/math")
+import { sum } from "./lib/math.js";
+
+// console.log(math.sum(1, 2));
+// console.log(math.sub(1, 2));
+
+console.log(sum(1, 2));
