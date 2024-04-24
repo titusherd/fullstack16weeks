21b927f9fce2371bb0479e7087da0ae2e16fc62b
Titus Herdiawan <76503729+titusherd@users.noreply.github.com>
2024-04-24 10:57:07 +0700

    add new folder: modularization

---
+++ typescript/modularization/main.ts
@@ -0,0 +1,4 @@
+const math = require("./src/lib/math")
+
+console.log(math.sum(1, 2));
+console.log(math.sub(1, 2));
---
+++ typescript/modularization/src/lib/math.ts
@@ -0,0 +1,9 @@
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+function sub(a: number, b: number): number {
+  return a - b;
+}
+
+module.exports = { sum, sub };
