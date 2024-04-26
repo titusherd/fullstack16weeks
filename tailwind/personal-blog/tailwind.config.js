9c5542b0bbbbbed6844aa7835332c01c5551e680
Titus Herdiawan <76503729+titusherd@users.noreply.github.com>
2024-04-26 14:13:42 +0700

    init

---
+++ tailwind/personal-blog/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["./src/index.html"], or
+  content: ["./src/**/*.{html,js}"], // scan all html files in src folder
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
