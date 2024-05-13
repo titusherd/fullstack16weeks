1e0f50215ae36d66581a582916bb6c8f9e284942
Titus Herdiawan <76503729+titusherd@users.noreply.github.com>
2024-05-13 20:17:07 +0700

    feat: create and get post

--- forumapp/src/routers/thread.router.js
+++ forumapp/src/routers/thread.router.js
@@ -1,46 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const Session = require('../models/session.model');
+const { createThread, getThreads } = require('../controllers/thread.controller');
 const threadRouter = express();
 // const jwt = require('jsonwebtoken');
 
-threadRouter.get('/thread', async (req, res) => {
-    res.send("thread");
-});
-
-// threadRouter.get('/thread', (req, res) => {
-//     const token = req.cookies.token;
-//     // res.send(token);
-
-//     try {
-//         const payload = jwt.verify(token, process.env.JWT_SECRET, {expiresIn: '7d'});
-//         // res.send(payload);
-//         console.log(payload);
-//         res.send("Ini data seluruh thread");
-//     } catch {
-//         res.status(401).send("Invalid token");
-//     }
-// });
-
-
-// threadRouter.get('/thread', async (req, res) => {
-//     // res.send("Ini data seluruh thread");
-
-//     const sessionId = req.cookies?.sessionId;
-
-//     if (!sessionId) {
-//         // return res.status(401).send("Unauthorized");
-//         res.send("Unauthorized")
-//     }
-
-//     const session = await Session.findOne({ _id: sessionId });
-
-//     if (!session) {
-//         // return res.status(401).send("Unauthorized");
-//         res.send("Unauthorized")
-//     }
-
-//     res.send("THREADS!!!!!!!")
-// });
+threadRouter.get('/thread/:id', getThreads);
+threadRouter.post('/thread', createThread);
 
 module.exports = threadRouter;
