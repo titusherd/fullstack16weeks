758e438d6c670344e03af49453b2a22da7b3346a
Titus Herdiawan <76503729+titusherd@users.noreply.github.com>
2024-05-13 11:29:45 +0700

    chore: models and type

--- backend-ts/src/index.ts
+++ backend-ts/src/index.ts
@@ -1,8 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { authRouter } from "./routers/auth.router";
 
+dotenv.config();
 const app = express();
 const port = 8000;
 
+mongoose.connect(process.env.MONGODB_URI as string);
+
+app.use(authRouter);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
