6f2c88f7dd67b20bc27901478d78a55e914baeeb
Titus Herdiawan <76503729+titusherd@users.noreply.github.com>
2024-05-15 16:28:35 +0700

    feat: register user

--- forumapp-ts/src/controllers/register.controller.ts
+++ forumapp-ts/src/controllers/register.controller.ts
@@ -1,5 +1,18 @@
 import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import { User } from "../models/user.model";
 
-export const registerHandler = (req: Request, res: Response) => {
-  res.send("Register!");
+export const registerHandler = async (req: Request, res: Response) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  await newUser.save();
+
+  res.status(201).send({ message: "User created!" });
 };
