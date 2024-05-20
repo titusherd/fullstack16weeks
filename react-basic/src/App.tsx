9f1765180a7cfb1505217e5ab22593a3390266e4
Titus Herdiawan <76503729+titusherd@users.noreply.github.com>
2024-05-20 11:25:58 +0700

    init project

---
+++ react-basic/src/App.tsx
@@ -0,0 +1,14 @@
+// Entry Point
+
+import { Footer } from "./components/footer";
+import { Header } from "./components/header";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <main>This is body</main>
+      <Footer />
+    </div>
+  );
+}
