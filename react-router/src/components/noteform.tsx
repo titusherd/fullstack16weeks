ae3fd267b50b7dcccb5a28058e488944f86f5f05
Titus Herdiawan <76503729+titusherd@users.noreply.github.com>
2024-05-29 23:03:19 +0700

    feat: user and email (learn useState())

--- react-router/src/components/noteform.tsx
+++ react-router/src/components/noteform.tsx
@@ -1,13 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const NoteForm = () => {
   const [counter, setCounter] = useState(0);
+  const [userName, setUserName] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [isUserValid, setIsUserValid] = useState(false);
+
+  function checkEmailPassword(userName: string, userPassword: string) {
+    if (userName === "admin" && userPassword === "admin") {
+      setIsUserValid(true);
+    } else {
+      setIsUserValid(false);
+    }
+  }
+  useEffect(() => {
+    console.log("useEffect called");
+    checkEmailPassword(userName, userPassword);
+  }, [userName, userPassword]);
+
+  // dependency array is to specify when the effect should run
+  // zero dependencies means that the effect will only run once after the first render
 
   return (
     <>
-      <div>Counter: {counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>Increment</button>
-      <button onClick={() => setCounter(counter - 1)}>Decrement</button>
+      <div>
+        <div>Counter: {counter}</div>
+        <button onClick={() => setCounter(counter + 1)}>Increment</button>
+        <button onClick={() => setCounter(counter - 1)}>Decrement</button>
+      </div>
+      <br />
+      <div>
+        <div>Username: {userName}</div>
+        <input
+          type="text"
+          placeholder="Enter your username"
+          onChange={(event) => setUserName(event.target.value)}
+        />
+        <div>Password: {userPassword}</div>
+        <input
+          type="password"
+          placeholder="Enter your password"
+          onChange={(e) => setUserPassword(e.target.value)}
+        />
+      </div>
+      <div>{isUserValid ? "Welcome admin" : "Invalid user"}</div>
     </>
   );
 };
--- react-router/src/main.tsx
+++ react-router/src/main.tsx
@@ -6,6 +6,9 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    {/* StrictMode is a tool for highlighting potential problems in an
+    application. Like Fragment, StrictMode does not render any visible UI. It
+    activates additional checks and warnings for its descendants. */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
