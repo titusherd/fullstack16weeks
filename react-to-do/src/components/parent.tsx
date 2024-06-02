733a3f44a779c102ac7efe28de9e7725b4ae0a16
Titus Herdiawan <76503729+titusherd@users.noreply.github.com>
2024-06-02 19:54:44 +0700

    learn decoupling and SoC (Separated of Concern)

--- react-to-do/src/components/parent.tsx
+++ react-to-do/src/components/parent.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Children1 } from "./chidren1";
 
 export const DataContext = React.createContext<string | null>(null);
 
 export const Parent = () => {
-  const username = "John Doe";
+  const [username, setUsername] = useState("");
 
   return (
     <>
-      <DataContext.Provider value={username}>
+      <DataContext.Provider value={{ username, setUsername }}>
         <div>Parent</div>
         <div>PARENT {username}</div>
         <Children1 />
